2aab92ada513314f6f47f1f9f4b47e0c081d64e9
Rosstin <rosstin@strivr.com>
2019-07-03 11:06:01 -0700

    barebones for sounds

---
+++ UNDERDREAM/Assets/_SCRIPTS/SoundRune.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRune : MonoBehaviour {
+
+    [SerializeField] private Sprite runeSprite;
+    //[SerializeField] private Audio sound;
+
+	void Update () {
+		
+	}
+}
