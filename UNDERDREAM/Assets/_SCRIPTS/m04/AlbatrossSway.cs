fc602405408e0104b57b006daf65dd37352e14be
Rosstin Murphy <asterazul@gmail.com>
2020-01-05 20:59:07 -0800

    albatross

---
+++ UNDERDREAM/Assets/_SCRIPTS/m04/AlbatrossSway.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlbatrossSway : MonoBehaviour
+{
+    [Header("Outlets")]
+    [SerializeField]
+    private GameObject swayee;
+
+    [Header("Animation Constants")]
+    [SerializeField]
+    private float maxValue;
+    [SerializeField] private float period;
+
+    private int dir = 1;
+    private float startingValue;
+    private float currentTime = 0;
+
+    private void Start()
+    {
+        startingValue = swayee.transform.localPosition.y;
+    }
+
+    void Set(Vector3 setee, Vector3 setVal)
+    {
+
+    }
+
+    void Update()
+    {
+        if (dir == 1)
+        {
+            currentTime += Time.deltaTime;
+
+            if (currentTime < period)
+            {
+                float timeRatio = currentTime / period;
+
+                Vector3 newValue =
+                    new Vector3(
+                        swayee.transform.localPosition.x,
+                        startingValue + timeRatio * maxValue,
+                        swayee.transform.localPosition.z);
+
+                swayee.transform.localPosition = newValue;
+            }
+            else
+            {
+                dir = -1;
+            }
+        }
+        else if (dir == -1)
+        {
+            currentTime -= Time.deltaTime;
+
+            if (currentTime > -period)
+            {
+                float timeRatio = currentTime / period;
+
+                Vector3 newValue =
+                    new Vector3(
+                        swayee.transform.localPosition.x,
+                        startingValue + timeRatio * maxValue,
+                        swayee.transform.localPosition.z);
+
+                swayee.transform.localPosition = newValue;
+            }
+            else
+            {
+                dir = 1;
+            }
+        }
+        else
+        {
+            Debug.LogError("direction should be 1 or -1: " + dir);
+        }
+
+
+    }
+}
---
+++ UNDERDREAM/Assets/_SCRIPTS/m04/Jitter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jitter : MonoBehaviour
+{
+    [SerializeField] private float jitterPeriod;
+    [SerializeField] private Vector3 jitter;
+
+    private float jitterElapsed = 0;
+    private Vector3 startingPosition;
+
+    void Start()
+    {
+        startingPosition = this.gameObject.transform.localPosition;
+    }
+
+    void Update()
+    {
+        jitterElapsed += Time.deltaTime;
+
+        if (jitterElapsed > jitterPeriod)
+        {
+            jitterElapsed = 0f;
+            this.gameObject.transform.localPosition = startingPosition
+                + new Vector3(
+                    Random.Range(-jitter.x, jitter.x),
+                    Random.Range(-jitter.y, jitter.y),
+                    Random.Range(-jitter.z, jitter.z));
+        }
+    }
+}
