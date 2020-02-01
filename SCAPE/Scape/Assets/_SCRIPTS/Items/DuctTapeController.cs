c84ee023bd7827fbb7416004c333ff44e0e25954
Rosstin Murphy <asterazul@gmail.com>
2020-02-01 14:49:55 -0800

    line drawing math

---
+++ SCAPE/Scape/Assets/_SCRIPTS/Items/DuctTapeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuctTapeController : MonoBehaviour
+{
+    [Header("Outlets")]
+    public Camera ArCamera;
+    public LaunchpadBController LaunchpadB;
+
+    [Header("Configurables")]
+    public float ClosenessThreshholdPixels;
+
+    void Update()
+    {
+        Vector2 screenPointOfSelf = ArCamera.WorldToScreenPoint(this.transform.position);
+        Vector2 screenPointOfLaunchpad = ArCamera.WorldToScreenPoint(LaunchpadB.transform.position);
+        if (Mathf.Abs(Vector2.Distance(screenPointOfLaunchpad, screenPointOfSelf)) < ClosenessThreshholdPixels)
+        {
+            //stick the tape
+        }
+    }
+}
---
+++ SCAPE/Scape/Assets/_SCRIPTS/StretchyTape/StretchyTape.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StretchyTape : MonoBehaviour
+{
+    [Header("Outlets")]
+    public StretchyTapeEnd EndA;
+    public StretchyTapeEnd EndB;
+    public StretchyTapeBody Body;
+
+    public Transform DebugEndAPos;
+    public Transform DebugEndBPos;
+
+    private float initialLength;
+    private Vector3 initialLocalScale;
+
+    void Start()
+    {
+        initialLength = Body.transform.localScale.x;
+        initialLocalScale = Body.transform.localScale;
+
+        DrawTapeBetween(DebugEndAPos.position, DebugEndBPos.position);
+
+    }
+
+    public void DrawTapeBetween(Vector3 a, Vector3 b)
+    {
+
+
+        // deform it
+        float length = Mathf.Abs(Vector3.Distance(a, b));
+        Body.transform.localScale = new Vector3(initialLocalScale.x, initialLocalScale.y, length);
+
+        // place at midpoint
+        Body.transform.position = (a + b) / 2f;
+
+        // rotate it // its forward transform should point to one endpoint
+        Body.transform.LookAt(a);
+    }
+
+
+
+}
