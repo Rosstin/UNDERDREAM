18536b3aac52cfbc9873f2598bf96a380a6157f8
Rosstin Murphy <asterazul@gmail.com>
2020-02-01 12:37:55 -0800

    cat hates cucumber

--- SCAPE/Scape/Assets/_SCRIPTS/Items/CucumberController.cs
+++ SCAPE/Scape/Assets/_SCRIPTS/Items/CucumberController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CucumberController : MonoBehaviour
 {
+    public Collider MyCollider;
+
     // Start is called before the first frame update
     void Start()
     {
--- SCAPE/Scape/Assets/_SCRIPTS/JellyCatController.cs
+++ SCAPE/Scape/Assets/_SCRIPTS/JellyCatController.cs
@@ -7,6 +7,9 @@ public class JellyCatController : MonoBehaviour
     [Header("Outlets")]
     public Camera ArCamera;
 
+    [Header("Outlets - Myself")]
+    public Collider MyCollider;
+
     [Header("Outlets - Items")]
     public CucumberController Cucumber;
 
@@ -15,8 +18,8 @@ public class JellyCatController : MonoBehaviour
     public float RotationSpeed;
     public float AverageDirectionChangePeriod;
     public float VariationInPeriod;
-    public float CucumberClosenessThreshhold;
-    public float CucumberCooldown;
+    public float CucumberClosenessThreshholdPixels;
+    public float BackupAmountMeters;
 
     private Vector3 FORWARD = new Vector3(0, 1, 0);
 
@@ -37,9 +40,22 @@ public class JellyCatController : MonoBehaviour
         curDirectionChangePeriod = AverageDirectionChangePeriod + Random.Range(-VariationInPeriod, +VariationInPeriod);
     }
 
+    private void BackupAndFlip()
+    {
+        BackupCat();
+        FlipCat();
+    }
+
+    // flip around and switch direction
     private void FlipCat()
     {
         this.transform.Rotate(0.0f, 180f, 0.0f, Space.Self);
+    }
+
+    // move back a bit so you're not colliding anymore
+    private void BackupCat()
+    {
+        this.transform.position += -this.transform.forward * BackupAmountMeters;
     }
 
     // Update is called once per frame
@@ -47,49 +63,46 @@ public class JellyCatController : MonoBehaviour
     {
         curTime += Time.deltaTime;
 
-        if(curTime > curDirectionChangePeriod)
+        if (curTime > curDirectionChangePeriod)
         {
             leftOrRight *= -1;
             curTime = 0f;
             NewDirectionChangePeriod();
         }
 
-        this.transform.Rotate(0.0f, leftOrRight* RotationSpeed * Time.deltaTime, 0.0f, Space.Self);
+        this.transform.Rotate(0.0f, leftOrRight * RotationSpeed * Time.deltaTime, 0.0f, Space.Self);
 
         this.transform.position += this.transform.forward * StartSpeed * Time.deltaTime;
 
         // bottom left is 0,0, bottom right is 0,1, top left is 1,0, top right is 1,1
-        Vector3 screenPointOfCat = ArCamera.WorldToScreenPoint(this.transform.position);
+        Vector2 screenPointOfCat = ArCamera.WorldToScreenPoint(this.transform.position);
         if (screenPointOfCat.x < 0f)
         {
-            Debug.Log("cat off screen to left");
-            FlipCat();
+            BackupAndFlip();
         }
         else if (screenPointOfCat.x > ArCamera.pixelWidth)
         {
-            Debug.Log("cat off screen to right");
-            FlipCat();
+            BackupAndFlip();
         }
 
         if (screenPointOfCat.y < 0f)
         {
-            Debug.Log("cat off screen to bot");
-            FlipCat();
+            BackupAndFlip();
         }
         else if (screenPointOfCat.y > ArCamera.pixelHeight)
         {
-            Debug.Log("cat off screen to top");
-            FlipCat();
+            BackupAndFlip();
         }
 
-        if(Mathf.Abs( Vector3.Distance(Cucumber.transform.position, this.transform.position)) < CucumberClosenessThreshhold)
+        Vector2 screenPointOfCucumber = ArCamera.WorldToScreenPoint(Cucumber.transform.position);
+
+
+
+
+        if (Mathf.Abs(Vector2.Distance(screenPointOfCat, screenPointOfCucumber)) < CucumberClosenessThreshholdPixels)
         {
-            Debug.Log("HISS! TOO close to CUCUMBER!");
-            FlipCat();
+            BackupAndFlip();
         }
-
-        // determine where the cat is in screen space
-        // find the distance of the cat to the top, the bottom, the left, and the right, do things with it
 
     }
 }
