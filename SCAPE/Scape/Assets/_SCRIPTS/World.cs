215852a9a441789c466a0c28c55e56420093f67d
Kellie Medlin <kellie.medlin@gmail.com>
2020-02-01 11:34:55 -0800

    Add initial spawner activation logic

--- SCAPE/Scape/Assets/_SCRIPTS/World.cs
+++ SCAPE/Scape/Assets/_SCRIPTS/World.cs
@@ -2,11 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WorldState {
+    INITIAL = 0,
+    PLAY_WITH_CAT,
+    ENDGAME,
+};
+
 public class World : Singleton<World>
 {
+    public WorldState state{
+    get{
+        return _state;
+    }
+    set{
+        WorldState oldState = _state;
+        _state = state;
+        SpawnManager.Instance.OnWorldStateChange(oldState, state);
+    }
+    }
+    private WorldState _state;
+
     protected World() {}
 
     private void Start()
     {
+        // Stage 1: wait for cat to spawn
+        // Stage 2: wait for other things to spawn
     }
 }
