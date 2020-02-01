2f338958fa2e06ed853718434959ffbc52d0e2ff
Kellie Medlin <kellie.medlin@gmail.com>
2020-02-01 10:54:01 -0800

    Add initial JellyCat world scene

---
+++ SCAPE/Scape/Assets/_SCRIPTS/World.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : Singleton<World>
+{
+    protected World() {}
+
+    private void Start()
+    {
+    }
+}
