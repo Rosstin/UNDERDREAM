f981c6d0667c78c2e1ce0e795b2fdab2434f9781
wincelet <brookelfnichols@gmail.com>
2020-02-02 10:52:30 -0800

    fixed boxable references

--- SCAPE/Scape/Assets/_SCRIPTS/JellyCatController.cs
+++ SCAPE/Scape/Assets/_SCRIPTS/JellyCatController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JellyCatController : MonoBehaviour
+public class JellyCatController : Boxable
 {
     [Header("Outlets")]
     public Camera ArCamera;
