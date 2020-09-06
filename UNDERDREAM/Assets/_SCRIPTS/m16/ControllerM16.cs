fff638b651a96ee7de88d4a759f8148da14eae7c
Rosstin Murphy <asterazul@gmail.com>
2020-09-06 13:29:19 -0700

    tweaks

--- UNDERDREAM/Assets/_SCRIPTS/m16/ControllerM16.cs
+++ UNDERDREAM/Assets/_SCRIPTS/m16/ControllerM16.cs
@@ -7,6 +7,9 @@ public class ControllerM16 : BaseController
     [Header("Cymbals")]
     public float CymbalTime;
     public AudioSource CymbalSFX;
+    public Camera Cam;
+    public Vector3 CamPosition2;
+    // pos1 is 0,0,-10
 
     private float elapsed;
     private bool playedCymbal = false;
@@ -22,6 +25,7 @@ public class ControllerM16 : BaseController
         {
             playedCymbal = true;
             CymbalSFX.Play();
+            Cam.gameObject.transform.position = CamPosition2;
         }
     }
 }
