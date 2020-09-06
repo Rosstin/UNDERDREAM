1fee9ca8530b6a6cefab528bdccf6d0123cc57ad
Rosstin Murphy <asterazul@gmail.com>
2020-09-05 20:14:13 -0700

    shipsmoke

--- UNDERDREAM/Assets/_SCRIPTS/m12/ControllerM12.cs
+++ UNDERDREAM/Assets/_SCRIPTS/m12/ControllerM12.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControllerM12 : MonoBehaviour
+public class ControllerM12 : BaseController
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Phase Times")]
+    public float[] Times;
+
+    [Header("Sprites")]
+    public GameObject NewHole;
+    public GameObject NewSmoke;
+    public GameObject Boat;
+
+    [Header("Sfx")]
+    public AudioSource CannonSound;
+
+    private float elapsed;
+    private bool[] flags = new []{false,false,false};
 
     // Update is called once per frame
     void Update()
     {
-        
+        BaseUpdate();
+
+        elapsed += Time.deltaTime;
+
+        if (elapsed > Times[0] && !flags[0])
+        {
+            flags[0] = true;
+            NewHole.SetActive(true);
+            CannonSound.Play();
+        }
+        else if (elapsed > Times[1] && !flags[1])
+        {
+            flags[1] = true;
+            NewSmoke.SetActive(true);
+        }
+        else if (elapsed > Times[2] && !flags[2])
+        {
+            flags[2] = true;
+            LoadNextScene();
+        }
+
     }
 }
