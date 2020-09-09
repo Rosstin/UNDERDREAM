4b706ab2ab30c65388490d0c87919626e67ab9a3
Rosstin Murphy <asterazul@gmail.com>
2020-09-09 07:50:29 -0700

    script for waves

---
+++ UNDERDREAM/Assets/_SCRIPTS/m17/ScrollingWaves.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingWaves : MonoBehaviour
+{
+    public float Speed;
+    public Vector2 MinMaxX;
+
+    public GameObject[] Waves;
+
+
+    void Update()
+    {
+        foreach (GameObject wave in Waves)
+        {
+            wave.transform.position +=
+                new Vector3(
+                    Speed * Time.deltaTime,
+                    0,
+                    0
+                );
+
+            if (wave.transform.position.x < MinMaxX.x)
+            {
+                wave.transform.position
+                    =
+                    new Vector3(
+                        MinMaxX.y,
+                        wave.transform.position.y,
+                        wave.transform.position.z
+                            );
+                    
+            }
+
+            if (wave.transform.position.x > MinMaxX.y)
+            {
+                wave.transform.position
+                    =
+                    new Vector3(
+                        MinMaxX.x,
+                        wave.transform.position.y,
+                        wave.transform.position.z
+                    );
+            }
+        }
+    }
+}
