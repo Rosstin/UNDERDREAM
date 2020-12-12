7c91ed5febce2ab73ba924bec012c214d2ee697c
Rosstin Murphy <asterazul@gmail.com>
2020-12-12 07:23:55 -0800

    add pbag

---
+++ UNDERDREAM/Assets/_SCRIPTS/m45/PunchingBag.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchingBag : Hurdle
+{
+    [SerializeField] private List<SpriteRenderer> damageSprites;
+
+    private int totalDamage = 0;
+
+    private void Start()
+    {
+        SetDamage(0);
+    }
+
+    public void TakeDamage()
+    {
+        SetDamage(totalDamage + 1);
+    }
+
+    public void SetDamage(int totalDam)
+    {
+        this.totalDamage = totalDam;
+        foreach (SpriteRenderer s in damageSprites)
+        {
+            s.gameObject.SetActive(false);
+        }
+
+        damageSprites[totalDam].gameObject.SetActive(true);
+    }
+
+}
