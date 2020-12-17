c4b2430a8f4609b2f82279127f4379d3d23ce393
Shanty <shanty@Shantys-MacBook-Pro.local>
2020-12-17 14:29:03 -0800

    aspect ratio tweaks

---
+++ UNDERDREAM/Assets/_SCRIPTS/m70/GameMasterM70.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMasterM70 : MonoBehaviour
+{
+    public UniversalDataSO Data;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Data.DestroyTigerSong();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
