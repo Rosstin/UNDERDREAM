47236b20eef9cb67c602a1f33e020913ed0cc351
Rosstin Murphy <asterazul@gmail.com>
2021-05-05 17:36:59 -0700

    m23

--- UNDERDREAM/Assets/_SCRIPTS/m23/ShantyM23.cs
+++ UNDERDREAM/Assets/_SCRIPTS/m23/ShantyM23.cs
@@ -11,7 +11,18 @@ public class ShantyM23 : MonoBehaviour
     public ShantyText P1Text;
     public ShantyText P2Text;
 
+    public Camera Cam1;
+    public Camera Cam2;
+
     public MaoxunM23 Moxie;
+
+    private void Start()
+    {
+        P1Text.gameObject.SetActive(true);
+        P2Text.gameObject.SetActive(false);
+        Cam1.gameObject.SetActive(true);
+        Cam2.gameObject.SetActive(false);
+    }
 
     public void GetBall()
     {
@@ -20,13 +31,8 @@ public class ShantyM23 : MonoBehaviour
         CaughtSFX.Play();
         P1Text.gameObject.SetActive(false);
         P2Text.gameObject.SetActive(true);
+        Cam1.gameObject.SetActive(false);
+        Cam2.gameObject.SetActive(true);
     }
 
-    public void Update()
-    {
-        if (P2Text.IsFinished())
-        {
-            Moxie.LoadNextScene();
-        }
-    }
 }
