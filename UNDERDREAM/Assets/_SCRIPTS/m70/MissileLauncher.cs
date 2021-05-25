06e8e3b77d160706e90cd83f84f43712d8fecd13
Rosstin Murphy <asterazul@gmail.com>
2021-05-24 17:23:50 -0700

    missile launcher ik works

--- UNDERDREAM/Assets/_SCRIPTS/m70/MissileLauncher.cs
+++ UNDERDREAM/Assets/_SCRIPTS/m70/MissileLauncher.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class MissileLauncher : MonoBehaviour
 {
+    [Header("Outlet: Top and Bottom Anchors")]
     [SerializeField] private Transform topAnchor;
     [SerializeField] private Transform botAnchor;
+
+    [Header("Top Front Housings")]
+    [SerializeField] private GameObject topFrontFoot;
+    [SerializeField] private GameObject topFrontSheath;
+    [SerializeField] private LineRenderer topFrontLR;
+
+    [Header("Top Back Housings")]
+    [SerializeField] private GameObject topBackFoot;
+    [SerializeField] private GameObject topBackSheath;
+    [SerializeField] private LineRenderer topBackLR;
+
+    [Header("Bot Front Housings")]
+    [SerializeField] private GameObject botFrontFoot;
+    [SerializeField] private GameObject botFrontSheath;
+    [SerializeField] private LineRenderer botFrontLR;
+
+    [Header("Bot Back Housings")]
+    [SerializeField] private GameObject botBackFoot;
+    [SerializeField] private GameObject botBackSheath;
+    [SerializeField] private LineRenderer botBackLR;
 
     private float upRatio = 0.5f;
 
@@ -19,5 +40,18 @@ public class MissileLauncher : MonoBehaviour
     void Update()
     {
         this.transform.position = Vector3.Lerp(botAnchor.position, topAnchor.position, upRatio);
+
+        topFrontLR.SetPosition(0, topFrontFoot.transform.position);
+        topFrontLR.SetPosition(1, topFrontSheath.transform.position);
+
+        topBackLR.SetPosition(0, topBackFoot.transform.position);
+        topBackLR.SetPosition(1, topBackSheath.transform.position);
+
+        botFrontLR.SetPosition(0, botFrontFoot.transform.position);
+        botFrontLR.SetPosition(1, botFrontSheath.transform.position);
+
+        botBackLR.SetPosition(0, botBackFoot.transform.position);
+        botBackLR.SetPosition(1, botBackSheath.transform.position);
+
     }
 }
