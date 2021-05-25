09e958deda024cd112eef387fa4d0d9fe7c969c3
Rosstin Murphy <asterazul@gmail.com>
2021-05-24 17:09:08 -0700

    missile launcher pos

---
+++ UNDERDREAM/Assets/_SCRIPTS/m70/MissileLauncher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileLauncher : MonoBehaviour
+{
+    [SerializeField] private Transform topAnchor;
+    [SerializeField] private Transform botAnchor;
+
+    private float upRatio = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(botAnchor.position, topAnchor.position, upRatio);
+    }
+}
