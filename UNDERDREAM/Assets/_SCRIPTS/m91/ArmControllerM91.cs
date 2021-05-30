2dda0ef3f11676cf2cd7a275ca3ed1010f359322
Rosstin Murphy <asterazul@gmail.com>
2021-05-30 10:23:19 -0700

    puck

---
+++ UNDERDREAM/Assets/_SCRIPTS/m91/ArmControllerM91.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ArmControllerM91 : BaseController
+{
+    [Header("Face Outlets")]
+    [SerializeField] private GameObject face;
+    [SerializeField] private GameObject puckerAnim;
+
+    [Header("Lemon")]
+    [SerializeField] private FlyingLemon lemon;
+
+    [Header("Arm")]
+    [SerializeField] [Range(0f, 1f)] private float armSpeed;
+    [SerializeField] private float maxHeight;
+
+    [Header("Arm Outlets")]
+    [SerializeField] private GameObject arm;
+    [SerializeField] private GameObject clenchedArm;
+
+    [Header("Jitter")]
+    [SerializeField] private float jitterPeriod;
+    [SerializeField] private Vector3 jitter;
+    [SerializeField] [Range(1, 10)] private int jitterIntensityIncreaseAfterClench;
+
+    [Header("Camera Jitter")]
+    [SerializeField] private Jitter camJitter;
+
+    private float jitterElapsed = 0f;
+    private bool clenched = false;
+
+    private bool startedClenched = false;
+
+    private float postClenchElapsed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        BaseUpdate();
+
+        jitterElapsed += Time.deltaTime;
+
+        if (jitterElapsed > jitterPeriod)
+        {
+            jitterElapsed = 0f;
+            this.gameObject.transform.localPosition += new Vector3(
+                    Random.Range(-jitter.x, jitter.x),
+                    Random.Range(-jitter.y, jitter.y),
+                    Random.Range(-jitter.z, jitter.z));
+        }
+
+        if (!clenched)
+        {
+            UpdatePreClench();
+        }
+        else
+        {
+            UpdatePostClench();
+        }
+
+    }
+
+
+    private void StartPostClench()
+    {
+        startedClenched = true;
+        face.gameObject.SetActive(false);
+        puckerAnim.gameObject.SetActive(true);
+        lemon.gameObject.SetActive(false);
+    }
+
+    void UpdatePostClench()
+    {
+        postClenchElapsed += Time.deltaTime;
+
+        if (!startedClenched)
+        {
+            StartPostClench();
+        }
+
+        /*
+        if (postClenchElapsed > nextSceneTime)
+        {
+            LoadNextScene();
+        }*/
+    }
+
+    void UpdatePreClench()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            arm.transform.localPosition =
+                new Vector3(
+                    arm.transform.localPosition.x,
+                    arm.transform.localPosition.y + Time.deltaTime * armSpeed,
+                    arm.transform.localPosition.z
+                    );
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            arm.transform.localPosition =
+                new Vector3(
+                    arm.transform.localPosition.x,
+                    arm.transform.localPosition.y - Time.deltaTime * armSpeed,
+                    arm.transform.localPosition.z
+                    );
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            arm.transform.localPosition =
+                new Vector3(
+                    arm.transform.localPosition.x - Time.deltaTime * armSpeed,
+                    arm.transform.localPosition.y,
+                    arm.transform.localPosition.z
+                    );
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            arm.transform.localPosition =
+                new Vector3(
+                    arm.transform.localPosition.x + Time.deltaTime * armSpeed,
+                    arm.transform.localPosition.y,
+                    arm.transform.localPosition.z
+                    );
+        }
+
+        if (arm.transform.position.y > maxHeight)
+        {
+            //clickSound.Play();
+
+            arm.SetActive(false);
+            clenchedArm.SetActive(true);
+
+            clenchedArm.transform.position = new Vector3(
+                arm.transform.position.x,
+                maxHeight,
+                arm.transform.position.z
+                );
+
+            clenched = true;
+
+            jitter *= jitterIntensityIncreaseAfterClench;
+            jitterPeriod /= jitterIntensityIncreaseAfterClench;
+        }
+    }
+
+
+
+}
