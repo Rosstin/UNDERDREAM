9280e50deba9031b5e52437eb3b85665d308ff69
Rosstin Murphy <asterazul@gmail.com>
2023-02-25 07:06:33 -0800

    poop

---
+++ TAMAMON-IDLE/Assets/_SCRIPTS/TamaDataSO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/TamaDataSO", order = 1)]
+public class TamaDataSO : ScriptableObject
+{
+
+
+
+
+}
+
+
+public class Era01Stats
+{
+    public int Courage = 0;
+    public int Venom = 0;
+}
