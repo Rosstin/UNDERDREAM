95cfb377afdc3e0c0b3a13e9232c40c7b32335bf
Rosstin Murphy <asterazul@gmail.com>
2023-07-29 14:03:08 -0700

    bars can be inflated from data

--- meta-quest-2-unity-starter-main/Assets/_SCRIPTS/Graph.cs
+++ meta-quest-2-unity-starter-main/Assets/_SCRIPTS/Graph.cs
@@ -9,28 +9,53 @@ public class Graph : MonoBehaviour
 {
     [Header("Configurable Graph Data")]
     public List<BarData> BarsData;
+    public float XScaleFactor; // convert between unity meters and the graph's units - 0.1 would mean that 1 unity meter is 10 graph units in the X
 
     [Header("Prefabs")]
     [SerializeField] private Bar barPrefab;
 
 
-    private List<Bar> bars;
+    private List<Bar> bars = new List<Bar>();
 
-    public void MakeBars()
+    private void Start()
     {
-        bars.Add(GameObject.Instantiate(barPrefab));
-
-        //for(int i = 0; i < )
-        
+        MakeBars();
     }
 
-    [System.Serializable]
-    public class BarData
+    private void MakeBars()
     {
-        public int Index;
-        public int Value;
+        // wipe existing bars
+        foreach(var bar in bars)
+        {
+            Destroy(bar.gameObject);
+        }
+
+        // initialize the list of bars
+        bars = new List<Bar>();
+
+        // inflate bars from data
+        for(int i = 0; i < BarsData.Count; i++)
+        {
+            // create the bar
+            bars.Add(Instantiate(barPrefab));
+
+            bars[i].transform.parent = this.transform;
+
+            // initialize it
+            bars[i].Init(BarsData[i]);
+
+            // place it
+            bars[i].transform.localPosition = new Vector3(BarsData[i].Index * XScaleFactor, 0f, 0f);
+        }
     }
 
 
+}
+
+[System.Serializable]
+public class BarData
+{
+    public int Index;
+    public int Value;
 }
 
