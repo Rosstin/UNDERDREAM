abf2fefaa603df59f6384282e753adc0dffd96ec
Rosstin Murphy <asterazul@gmail.com>
2023-07-31 06:48:39 -0700

    rename, delete

--- meta-quest-2-unity-starter-main/Assets/_SCRIPTS/IMoveable.cs
+++
@@ -1,8 +0,0 @@
-
-/// <summary>
-/// An interface for objects that can be moved by the player
-/// </summary>
-public interface IMoveable
-{
-    // todo: events for picking up and dragging the object - look at the example package
-}
