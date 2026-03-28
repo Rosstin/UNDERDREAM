28f30e7b6f319d42fe841f905679f423897c4bb8
Rosstin <rosstin.murphy@gmail.com>
2026-03-28 09:52:26 -0700

    todo write random level gen for bobarista

--- PuzzlePig/Assets/SCRIPTS/Bobarista/GemsContainer.cs
+++ PuzzlePig/Assets/SCRIPTS/Bobarista/GemsContainer.cs
@@ -6,6 +6,10 @@ public class GemsContainer : MonoBehaviour
 {
     private const int LONG_WIDTH = 5; // width of the "long" side
 
+    public float gemDiameter; // should be 1
+
+    public float zDistance;
+    
 
     public int GetShortWidth()
     {
@@ -15,7 +19,25 @@ public class GemsContainer : MonoBehaviour
     public Vector3 GetPositionForIndex(int rowFromTop, int columnFromLeft)
     {
         // return the real worldspace position of a gem based on its location
+        // given the radius of the gem, position them
+        // odd rows, even rows
+        // you can pack them in tighter if you want
+        
+        bool isOdd = rowFromTop % 2 == 1;
 
+        float oddOffset = 0;
+        if (isOdd)
+        {
+            oddOffset = gemDiameter / 2f;
+        }
+        
+        
+        Vector3 position = new Vector3(columnFromLeft * gemDiameter, +oddOffset -rowFromTop * gemDiameter, zDistance);
+        
+        // snap them to nearest pos
+        
+        
+        
         Vector3 gemPos = new Vector3();
 
         return gemPos;
