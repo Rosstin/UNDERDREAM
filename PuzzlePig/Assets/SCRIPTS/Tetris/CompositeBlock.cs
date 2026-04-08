469f7b93cd74f8778d8738c8a994964753e239db
Rosstin <rosstin.murphy@gmail.com>
2026-04-08 16:41:54 -0700

    generate 3x3 blocks

--- PuzzlePig/Assets/SCRIPTS/Tetris/CompositeBlock.cs
+++ PuzzlePig/Assets/SCRIPTS/Tetris/CompositeBlock.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class CompositeBlock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Prefabs")]
+    [SerializeField] public Shard shardPrefab;
+
+    private List<Shard> myShards = new List<Shard>(); 
+    
+    public void GenerateRandomBlock()
     {
+        // for starters, let's generate a 3x3 grid of colors. Then let's simplify the shards
+        
+        // generating a 3x3
+        for (int x = 0; x < 3; x++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                TetrisGS.ShardFlavors randomFlavor = GenerateRandomFlavor();
+            }
+        }
+        
+        
+        
+        
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Generate one of 9 random flavors. Todo: more detailed params
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    private TetrisGS.ShardFlavors GenerateRandomFlavor()
     {
-        
+        throw new System.NotImplementedException();
     }
 }
