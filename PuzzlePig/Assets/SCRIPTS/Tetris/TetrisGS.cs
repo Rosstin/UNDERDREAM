48a5a969e4419e753a97c4231ff626cf8befaf26
Rosstin Murphy <rossmurphy@meta.com>
2026-04-09 09:57:59 -0700

    intersection plane for tetris is func

--- PuzzlePig/Assets/SCRIPTS/Tetris/TetrisGS.cs
+++ PuzzlePig/Assets/SCRIPTS/Tetris/TetrisGS.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class TetrisGS : MonoBehaviour
+public class TetrisGS : BaseController
 {
     [Header("Prefabs")] 
     public CompositeBlock compPrefab;
@@ -22,6 +22,14 @@ public class TetrisGS : MonoBehaviour
     public Material rubMat;
     public Material sapMat;
     public Material tangMat;
+
+    [Header("Outlets")]
+    public Camera mainCamera;
+    public Collider CollisionPlane;
+    public TetrisRet heldRet = null;
+    
+    [Header("Configs")]
+    public Vector3 startPos;
 
     public enum ShardFlavors
     {
@@ -76,8 +84,19 @@ public class TetrisGS : MonoBehaviour
 
     private void Update()
     {
+        BaseUpdate();
+
+        UpdateTetrisControls();
+    }
+
+    private void UpdateTetrisControls()
+    {
+                
+        
         if (Input.GetKeyDown(KeyCode.F1))
         {
+            Debug.Log("random block ");
+            
             ClearBlocks();
             
             CompositeBlock cb = GameObject.Instantiate(compPrefab).GetComponent<CompositeBlock>();
@@ -89,6 +108,70 @@ public class TetrisGS : MonoBehaviour
             
         }
 
+        if (CommandsStartedThisFrame.ContainsKey(Command.Fire))
+        {
+
+            
+            RaycastHit hit;
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+            
+                TetrisRet bobaRet = objectHit.gameObject.GetComponent<TetrisRet>();
+
+                if (bobaRet != null)
+                {
+                    heldRet = bobaRet;
+                }
+            }
+                    
+        }
+        
+        if (heldRet != null)
+        {
+            // dropping ing
+            if (!CommandsHeldThisFrame.ContainsKey(Command.Fire))
+            {
+                // calculate the direction between the current position and the startpos
+                Vector3 shootDirection = startPos - heldRet.transform.position;
+                
+                shootDirection.Normalize();
+
+                //DummyBall.transform.position = startPos + shootDirection * fireDist;
+                
+                /*
+                DummyBall.transform.position = startPos;
+                
+                DummyBall.Shoot(shootDirection, speed);
+                */
+                
+                
+                
+                
+                // we want to actually fire the ball, have it bounce from walls, stop when it hits another ball
+                
+                
+                heldRet.transform.position = startPos;
+                heldRet = null;
+            }
+            //holding ing
+            else
+            {
+
+                Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+                
+                RaycastHit hit;
+                
+                if(CollisionPlane.Raycast(ray, out hit, 100f))
+                {
+
+                    heldRet.transform.position = hit.point;
+                }
+            }
+                    
+        }
     }
 
     private void ClearBlocks()
